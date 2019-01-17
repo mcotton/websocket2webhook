2a200537bda17d2b29792a7b7d5ddfa247ec0feb
Mark Cotton <tifosigoon@gmail.com>
2019-01-16 23:09:03 -0600

    first couple calls are working

--- app.js
+++ app.js
@@ -7,7 +7,60 @@ var r           =       require('request'),
 
 var queue       =       kue.createQueue();
 
+var cookie_jar  =       r.jar();
 
+var  out        =       console.log,
+     config     =       require('./config'),
+     een        =       require('./een'),
+     host       =       'https://login.eagleeyenetworks.com';
+
+var curr_user   =       {},
+    users       =       {},
+    devices     =       {},
+    cameras     =       {},
+    bridges     =       {};
+
+
+var starting_lineup = [
+    function() { een.login({'username': config.username, 'password': config.password}, postLogin, failure); },
+    function() { een.getDeviceList({}, postGetDevices, failure); }
+];
+
+
+function execute_next_step() {
+    if(starting_lineup.length > 0)  { 
+        starting_lineup.shift()();
+    } else {
+        console.log("Error: execute_next_step ran out of starting_lineup items")
+    }
+}
+
+
+function postLogin(data) { 
+    console.log(data.statusCode + ': successfully logged-in');
+    cookie_jar = een.export_cookie_jar();
+    curr_user = data.body;
+    execute_next_step();
+}
+
+
+function postGetDevices(data) {
+    console.log(data.statusCode + ': successfully got device list');
+    devices = data.body;
+    u.each(devices, function(item) {
+        if(item[3] === 'camera') {
+            cameras.push(item);
+        } else if (item[3] === 'bridge') {
+            bridges.push(item);
+        }
+    });
+    execute_next_step();
+}
+
+
+function failure(data) {
+    console.log(data.statusCode + ': failure handler');
+}
 
 
 
@@ -17,3 +70,10 @@ var app = express();
 app.use( kue.app );
 app.listen( 3000 );
 console.log( 'UI started on port 3000' );
+
+
+execute_next_step();
+
+
+
+
---
+++ een.js
@@ -0,0 +1,278 @@
+var request = require('request'),
+    cookie_jar = request.jar(),
+    host = 'https://api.eagleeyenetworks.com';
+
+// make the cookie jar available outside this module
+exports.cookie_jar = cookie_jar;
+
+exports.login = function(opts, success, failure) {
+    request.post({
+        url: host + '/g/aaa/authenticate',
+        jar: cookie_jar,
+        json: true,
+        body: {
+            'username': opts.username,
+            'password': opts.password,
+            'realm': opts.realm || 'eagleeyenetworks'
+        }
+        }, function(err, res, body) {
+            if (err) { console.log(err,res,body); }
+            if (!err && res.statusCode == 200) {
+                request.post({
+                    url: host + '/g/aaa/authorize',
+                    jar: cookie_jar,
+                    json: true,
+                    body: { token: res.body.token }
+                    }, function(err, res, body) {
+                            if (err) { throw new Error('Authorize error') }
+                            if (!err && res.statusCode == 200) {
+                                // call success callback with user object
+                                if ( typeof success === 'function') success(res);
+                        }
+                })
+            } else {
+                // call failure callback with status code
+                if ( typeof failure === 'function') failure(res);
+            }
+        }
+    )
+};
+
+exports.getImage = function(opts, success, failure) {
+    var img_url = [   host,
+                    '/asset/asset/image.jpeg?c=', opts.c,
+                    ';t=', opts.ts || 'now',
+                    ';q=', opts.q || 'high',
+                    ';a=', opts.a || 'all'
+                    ].join('')
+    console.log('Requesting image: ' + img_url)
+    return  request.get({
+                    url: img_url,
+                    jar: cookie_jar
+                },
+                function (err, res, body) {
+                    if (err) { return err }
+                    if (!err && res.statusCode == 200) {}
+                    return res
+                }
+            )
+
+};
+
+
+exports.getPrevImage = function(opts, success, failure) {
+    var img_url = [   host,
+                    '/asset/prev/image.jpeg?c=', opts.c,
+                    ';t=', opts.ts || 'now',
+                    ';q=', opts.q || 'high',
+                    ';a=', opts.a || 'all'
+                    ].join('')
+    console.log('Requesting previous image: ' + img_url)
+    return  request.get({
+                    url: img_url,
+                    jar: cookie_jar
+                },
+                function (err, res, body) {
+                    if (err) { return err }
+                    if (!err && res.statusCode == 200) {}
+                    return res
+                }
+            )
+
+};
+
+exports.getNextImage = function(opts, success, failure) {
+    var img_url = [   host,
+                    '/asset/next/image.jpeg?c=', opts.c,
+                    ';t=', opts.ts || 'now',
+                    ';q=', opts.q || 'high',
+                    ';a=', opts.a || 'all'
+                    ].join('')
+    console.log('Requesting next image: ' + img_url)
+    return  request.get({
+                    url: img_url,
+                    jar: cookie_jar
+                },
+                function (err, res, body) {
+                    if (err) { return err }
+                    if (!err && res.statusCode == 200) {}
+                    return res
+                }
+            )
+
+};
+
+exports.getAfterImage = function(opts, success, failure) {
+    var img_url = [   host,
+                '/asset/after/image.jpeg?c=', opts.c,
+                ';t=', opts.ts || 'now',
+                ';q=', opts.q || 'high',
+                ';a=', opts.a || 'all'
+                ].join('')
+    console.log('Requesting after image: ' + img_url)
+    return  request.get({
+                url: img_url,
+                jar: cookie_jar
+            },
+            function (err, res, body) {
+                if (err) { return err }
+                if (!err && res.statusCode == 200) {}
+                return res
+            }
+        )
+}
+
+exports.getVideo = function(opts, success, failure) {
+    var src_url = [   host,
+                    '/asset/play/video.mp4?c=', opts.c,
+                    ';T=', opts.ts || 'now',
+                    ';e=', opts.e || 'event',
+                    ';q=', opts.a || 'low',
+                    ';d=', opts.d || '1'	//download video by default
+                    ].join('')
+    console.log('Requesting video: ' + src_url)
+    return  request.get({
+                    url: src_url,
+                    jar: cookie_jar
+                },
+                function (err, res, body) {
+                    if (err) { return err }
+                    if (!err && res.statusCode == 200) {}
+                    return res
+                }
+            )
+
+};
+
+
+exports.getImageList = function(opts, success, failure) {
+
+};
+
+exports.getVideoList = function(opts, success, failure) {
+    var img_url = [   host,
+                    '/asset/list/video?c=', opts.c,
+                    ';start_timestamp=', opts.start || 'now',
+                    ';end_timestamp=', opts.end || 'now',
+                    ';q=', opts.q || 'high',
+                    ';a=', opts.a || 'all'
+                    ].join('')
+    console.log('Requesting list of videos: ' + img_url)
+    return  request.get({
+                    url: img_url,
+                    jar: cookie_jar
+                },
+                function (err, res, body) {
+                    if (err) { return err }
+                    if (!err && res.statusCode == 200) {
+                        //console.log('Dumping output from getVideoList before calling success')
+                        //console.log(body)
+                        //console.log(res.statusCode)
+                        if(success) {
+                            success(res, body) 
+                        }
+                    }
+                    return res
+                }
+            )
+ 
+};
+
+exports.getDeviceList = function(opts, success, failure) {
+    var src_url = [ host, '/g/device/list'].join('')
+
+    //console.log('making request to getDeviceList: ' + src_url)
+    request.get({
+            url: src_url,
+            jar: cookie_jar
+        },
+        function (err, res, body) {
+            if (err) { if ( typeof failure === 'function') failure(res); }
+            if (!err && res.statusCode == 200) {
+                if ( typeof success === 'function') success(res);
+            }
+        }
+    )
+
+};
+
+exports.startPolling = function(opts, success, failure) {
+
+};
+
+exports.continuePolling = function(opts, success, failure) {
+
+};
+
+// archvm01.eagleeyenetworks.com/annotate?c=10042ffd&ts=20140322000104.000&ns=1
+exports.addAnnotations = function(opts, success, failure) {
+        console.log( [  'http:/','/archvm01.eagleeyenetworks.com', '/annotate?c=', opts.c,
+                '&ts=', opts.t, 
+                '&ns=', opts.ns 
+            ].join(''))
+    request.put({
+        url: [  'http:/','/archvm01.eagleeyenetworks.com', '/annotate?c=', opts.c,
+                '&ts=', opts.ts, 
+                '&ns=', opts.ns 
+            ].join(''),
+        jar: cookie_jar,
+        json: true,
+        body: opts.body
+        },
+        function(err, res, body) {
+            console.log('return from /annotate:', res.statusCode)
+            if(err) { return err }
+            if (!err && res.statusCode == 200) {
+                if ( typeof success === 'function') success(res.body);
+            } else {
+                if(typeof failure === 'function') failure(err, res, body);
+            }
+            return body;
+        }
+    )
+};
+
+
+exports.DtoS = function(epoch_time) {
+    var yy, mm, dd, hr, mn, sc, ms, timecode, jstime;
+        jstime = new Date(epoch_time);
+        yy = jstime.getUTCFullYear();
+        mm = this._padTo2Digits(1 + jstime.getUTCMonth());
+        dd = this._padTo2Digits(jstime.getUTCDate());
+        hr = this._padTo2Digits(jstime.getUTCHours());
+        mn = this._padTo2Digits(jstime.getUTCMinutes());
+        sc = this._padTo2Digits(jstime.getUTCSeconds());
+        ms = jstime.getUTCMilliseconds();
+        if (ms < 10) {
+            ms = '00' + ms;
+        } else if (ms < 100) {
+            ms = '0' + ms;
+        }
+        timecode = yy + mm + dd + hr + mn + sc + '.' + ms;
+        return timecode;
+};
+
+exports._padTo2Digits = function(num) {
+    return (((num < 10) ? '0' : '') + num);
+};
+
+exports.StoD = function (timecode) {
+    if(timecode) {
+        var yy, mm, dd, hr, mn, sc, ms, jstime;
+        yy = parseInt(timecode.substring(0, 4), 10);
+        mm = parseInt(timecode.substring(4, 6), 10);
+        dd = parseInt(timecode.substring(6, 8), 10);
+        hr = parseInt(timecode.substring(8, 10), 10);
+        mn = parseInt(timecode.substring(10, 12), 10);
+        sc = parseInt(timecode.substring(12, 14), 10);
+        ms = parseInt(timecode.substring(15), 10);
+        jstime = new Date(Date.UTC(yy, mm - 1, dd, hr, mn, sc, ms));
+        return jstime.valueOf();
+    }
+};
+
+exports.export_cookie_jar = function() {
+    return cookie_jar;
+}
+
+
