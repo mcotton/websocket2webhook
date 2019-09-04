0b450dedc18346e33f738f8266f1e41c5b709865
Mark Cotton <tifosigoon@gmail.com>
2019-09-04 15:09:23 -0500

    dependency update, added redis to docker-compose, cleaned up worker.js logging

--- worker.js
+++ worker.js
@@ -7,49 +7,38 @@ exports.doSomething = function(job, done) {
     // this is where the magic happens
     // remember to call done()
 
-    request.post({
-        url: config.webhook_url,
-        json: true,
-        body: job.data
-        }, function(err, res, body) {
-            if (err) { console.log(err,res,body); }
-            if (!err) {
-                if(config.debug) console.log('Called webhook and received status code: ' + res.statusCode);
-                switch(res.statusCode) {
-                    case 200:
-                        done();
-                        break;
-                    case 301:
-                    case 302:
-                    case 400:
-                    case 401:
-                    case 404:
-                    case 500:
-                    case 502:
-                    case 503:
-                    default:
-                        done(new Error(res.statusCode));
-                        break;
-                }
-                
-            }
-        });
+    if(config.debug) {
+        console.log("pulling item off of queue...");
+    }
+    done();
 
 };
 
 
 exports.onComplete = function(job, result) {
+    if(config.debug) {
+        console.log("...done");
+    }
 
 };
 
 exports.onFailedAttempt = function(job, errorMessage, doneAttempts) {
+    if(config.debug) {
+        console.log("...failed attempt...");
+    }
 
 };
 
 exports.onFailed = function(job, errorMessage) {
+    if(config.debug) {
+        console.log("...failed");
+    }
     
 };
 
 exports.onProgress = function(job, progress, data) {
+    if(config.debug) {
+        console.log("...progress is happening...");
+    }
     
 };
